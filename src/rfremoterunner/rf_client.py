37a21552f923767c638bb23f234d28643670a553
jayvynsong <songxiaojie_2005@163.com>
2023-03-07 14:09:59 +0800

    Fix errors in logger.

--- src/rfremoterunner/rf_client.py
+++ src/rfremoterunner/rf_client.py
@@ -10,11 +10,11 @@ from robot.utils.robotpath import find_file
 
 from rfremoterunner.utils import normalize_xmlrpc_address, calculate_ts_parent_path, read_file_from_disk
 
-logging.basicConfig(format='%(asctime)-15s  %(levelname)-10s  %(__name__)-10s  %(message)s', level=logging.INFO, filename=f'{__name__}.log' )
+logging.basicConfig(format='%(asctime)-15s  %(levelname)-10s  %(message)s', level=logging.INFO, filename=f'{__name__}.log' )
 logger = logging.getLogger(__file__)
 DEFAULT_PORT = 1471
-# IMPORT_LINE_REGEX = re.compile('(Resource|Library)([\\s]+)([^[\\n\\r]*)([\\s]+)')
-IMPORT_LINE_REGEX = re.compile('(Resource|Library)([\\s]+)([^[\\n\\r\\s]*)(.+)')
+IMPORT_LINE_REGEX = re.compile('(Resource|Library)([\\s]+)([^[\\n\\r]*)([\\s]+)')
+# IMPORT_LINE_REGEX = re.compile('(Resource|Library)([\\s]+)([^[\\n\\r\\s]*)(.+)')
 
 
 class RemoteFrameworkClient:
--- src/rfremoterunner/rf_server.py
+++ src/rfremoterunner/rf_server.py
@@ -10,7 +10,7 @@ from robot.run import run
 
 from rfremoterunner.utils import write_file_to_disk, read_file_from_disk
 
-logging.basicConfig(format='%(asctime)-15s  %(levelname)-10s  %(__name__)-10s  %(message)s', level=logging.INFO, filename=f'{__name__}.log' )
+logging.basicConfig(format='%(asctime)-15s  %(levelname)-10s  %(message)s', level=logging.INFO, filename=f'{__name__}.log' )
 logger = logging.getLogger(__file__)
 
 DEFAULT_ADDRESS = '0.0.0.0'
