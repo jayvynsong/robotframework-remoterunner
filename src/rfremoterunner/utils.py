4bda1464b35a5076a77429b30b8eac761925d4f8
jayvynsong <songxiaojie_2005@163.com>
2023-03-08 10:11:45 +0800

    Unpack archive once received at server side.

--- src/rfremoterunner/utils.py
+++ src/rfremoterunner/utils.py
@@ -2,9 +2,13 @@ from io import open
 import os
 import re
 import six
-from shutil import make_archive
+from shutil import make_archive, unpack_archive
 from base64 import b64encode, b64decode
+import logging
 
+logging.basicConfig(format='%(asctime)-15s  %(levelname)-10s  %(message)s', level=logging.INFO, filename=f'{__name__}.log' )
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG if debug else logging.INFO)
 
 PORT_INC_REGEX = '.*:[0-9]{1,5}$'
 
@@ -29,6 +33,7 @@ def read_file_from_disk(path, encoding='utf-8', into_lines=False):
     :return: Contents of the file
     :rtype: str
     """
+    logger.debug(f'read_file_from_disk - path: {path}')
     with open(path, 'r', encoding=encoding) as file_handle:
         return file_handle.readlines() if into_lines else file_handle.read()
 
@@ -43,9 +48,10 @@ def read_binary_from_disk(path, into_lines=False):
     :type into_lines: bool
 
     :return: Contents of the file
-    :rtype: str
+    :rtype: str, Base64 encoded.
     """
     filename = os.path.basename(os.path.normpath(path))
+    logger.debug(f'read_binary_from_disk - filename: {filename}, path: {path}')
     zipfile = make_archive(f'{filename}', 'zip', base_dir=f'{path}')
     with open(zipfile, 'rb') as file_handle:
         ret =  file_handle.readlines() if into_lines else b64encode(file_handle.read())
@@ -64,6 +70,7 @@ def write_file_to_disk(path, file_contents, encoding='utf-8'):
     :param encoding: Encoding of the file
     :type encoding: str
     """
+    logger.debug(f'write_file_to_disk - path: {path}')
     with open(path, 'w', encoding=encoding) as file_handle:
         file_handle.write(unicode(file_contents))
 
@@ -77,12 +84,11 @@ def write_binary_to_disk(path, file_contents):
     :param file_contents: Contents of the file
     :type file_contents: str | unicode
     """
-    filename = os.path.basename(os.path.normpath(path))
-    zipfile = make_archive(f'{filename}', 'zip', base_dir=f'{path}')
-    with open(zipfile, 'wb') as file_handle:
+    logger.debug(f'write_binary_to_disk - path: {path}')
+    with open(path, 'wb') as file_handle:
         file_handle.write(b64decode(file_contents))
         file_handle.close()
-    os.remove(zipfile)
+    unpack_archive(path, format='zip')
 
 def normalize_xmlrpc_address(address, default_port):
     """
